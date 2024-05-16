b3767eb223a0e7ad7e74c4a716c85fe9f6900e8b
Shailendra Kumar <74320112+softwaredev-sk@users.noreply.github.com>
2024-05-17 03:11:09 +0530

    fix url

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -4,7 +4,7 @@ export default function Error({ error, reset }) {
   return (
     <div className="error">
       <h2>Something went wrong! Please Try again later.</h2>
-      <p>Possible Error: {error.message}</p>
+      <p>Possible Error: {error}</p>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
