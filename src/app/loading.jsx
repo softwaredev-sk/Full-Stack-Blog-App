49ce476cedefcafb3187114cf0858cf32bf2c955
Shailendra Kumar <74320112+softwaredev-sk@users.noreply.github.com>
2024-05-23 11:12:29 +0530

    add appwide loading display

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import ActionStatus from '@/components/ActionStatus/ActionStatus';
+
+export default function Loading() {
+  return (
+    // <span>
+    <ActionStatus status="loading" iconSize="150" />
+    // </span>
+  );
+}
