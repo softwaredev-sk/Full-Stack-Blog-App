cbee7bba43d9bc318f6c2b27dd88093659993f3d
Shailendra Kumar <74320112+softwaredev-sk@users.noreply.github.com>
2024-05-29 18:54:54 +0530

    add mailer for contact me

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,6 +1,20 @@
 import { getAuthSession } from '@/utils/auth';
 import { NextResponse } from 'next/server';
 import prisma from '@/utils/connect';
+import { mailOptions, transporter } from '@/utils/mailerConfig';
+
+function generateHtml(body, sessionEmail) {
+  const html = `${
+    sessionEmail === 'notloggedin@notloggedin.com'
+      ? `<h2>You have got a mail from an unknown user named '${body.name}' with the mail id ${body.email}!</h2>`
+      : `<h2>Registered user with the email id ${sessionEmail} sent you a mail as ${body.email}!</h2>`
+  } \n
+  <p><b>Name:</b>  ${body.name}</p>
+  <p><b>Email:</b> ${body.email}</p>
+  <p><b>Message:</b> ${body.content}</p>
+  `;
+  return html;
+}
 
 export const POST = async (req) => {
   const session = await getAuthSession();
@@ -12,6 +26,12 @@ export const POST = async (req) => {
 
   try {
     const body = await req.json();
+    await transporter.sendMail({
+      ...mailOptions,
+      subject: `${body.email} says: ${body.subject}`,
+      text: body.content,
+      html: generateHtml(body, sessionEmail),
+    });
     await prisma.contactme.create({
       data: { ...body, userEmail: sessionEmail },
     });
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react';
 import styles from './Contact.module.css';
 import Image from 'next/image';
 import { AnimatePresence, motion } from 'framer-motion';
+import ActionStatus from '../ActionStatus/ActionStatus';
 
 function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -13,12 +14,26 @@ function validateEmail(email) {
   return true;
 }
 
+async function sendContactForm(data) {
+  const res = await fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to send message');
+  }
+  return res;
+}
+
 export default function Contact() {
   const emailRef = useRef();
   const subjectRef = useRef();
   const contentRef = useRef();
+  const nameRef = useRef();
 
   const [formCustomData, setFormCustomData] = useState({
+    name: '',
     email: '',
     subject: '',
     content: '',
@@ -35,40 +50,46 @@ export default function Contact() {
     });
   }
 
+  function handleFormSendStatus(val) {
+    setFormSuccessful(val);
+    lastTimer.current = setTimeout(() => {
+      lastTimer.current = null;
+      setFormSuccessful(null);
+    }, 2000);
+  }
+
   async function handleSubmit(e) {
+    handleFormSendStatus('');
     if (lastTimer.current) {
       clearTimeout(lastTimer.current);
     }
     e.preventDefault();
+    const name = formCustomData.name;
     const email = formCustomData.email;
     const subject = formCustomData.subject;
     const content = formCustomData.content;
-    if (validateEmail(email) && subject.trim() && content.trim()) {
-      const res = await fetch('/api/contact', {
-        method: 'POST',
-        body: JSON.stringify(formCustomData),
-      });
-
+    if (
+      name.trim() &&
+      validateEmail(email) &&
+      subject.trim() &&
+      content.trim()
+    ) {
+      const res = await sendContactForm(formCustomData);
       if (!res.ok) {
         console.log('failed to submit form');
+        handleFormSendStatus(false);
         return;
       }
       setFormCustomData({
+        name: '',
         email: '',
         subject: '',
         content: '',
       });
-      setFormSuccessful(true);
-      lastTimer.current = setTimeout(() => {
-        lastTimer.current = null;
-        setFormSuccessful(null);
-      }, 2000);
+
+      handleFormSendStatus(true);
     } else {
-      setFormSuccessful(false);
-      lastTimer.current = setTimeout(() => {
-        lastTimer.current = null;
-        setFormSuccessful(null);
-      }, 2000);
+      handleFormSendStatus(false);
     }
   }
 
@@ -76,36 +97,77 @@ export default function Contact() {
     <div>
       <h1>Contact Me</h1>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className={styles.input}
-          required
-          onChange={() => handleCustomForm('email', emailRef.current.value)}
-          ref={emailRef}
-          value={formCustomData.email}
-        ></input>
-        <input
-          type="text"
-          placeholder="Subject"
-          name="subject"
-          className={styles.input}
-          required
-          onChange={() => handleCustomForm('subject', subjectRef.current.value)}
-          ref={subjectRef}
-          value={formCustomData.subject}
-        ></input>
-        <textarea
-          type="text"
-          placeholder="..."
-          name="content"
-          className={styles.input}
-          required
-          onChange={() => handleCustomForm('content', contentRef.current.value)}
-          ref={contentRef}
-          value={formCustomData.content}
-        ></textarea>
+        <div className={styles.fields}>
+          <label htmlFor="name" className={styles.label}>
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Your Name"
+            name="name"
+            className={styles.input}
+            required
+            onChange={() => handleCustomForm('name', nameRef.current.value)}
+            ref={nameRef}
+            value={formCustomData.name}
+          ></input>
+        </div>
+
+        <div className={styles.fields}>
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            name="email"
+            className={styles.input}
+            required
+            onChange={() => handleCustomForm('email', emailRef.current.value)}
+            ref={emailRef}
+            value={formCustomData.email}
+          ></input>
+        </div>
+
+        <div className={styles.fields}>
+          <label htmlFor="subject" className={styles.label}>
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            placeholder="Subject"
+            name="subject"
+            className={styles.input}
+            required
+            onChange={() =>
+              handleCustomForm('subject', subjectRef.current.value)
+            }
+            ref={subjectRef}
+            value={formCustomData.subject}
+          ></input>
+        </div>
+
+        <div className={styles.fields}>
+          <label htmlFor="content" className={styles.label}>
+            Got to say something?
+          </label>
+          <textarea
+            type="text"
+            id="content"
+            placeholder="...?"
+            name="content"
+            className={styles.input}
+            required
+            onChange={() =>
+              handleCustomForm('content', contentRef.current.value)
+            }
+            ref={contentRef}
+            value={formCustomData.content}
+          ></textarea>
+        </div>
         <div className={styles.buttonContainer}>
           <div>
             <AnimatePresence>
@@ -130,8 +192,15 @@ export default function Contact() {
                   className={styles.msg}
                   exit={{ opacity: [1, 0], transition: { duration: 2 } }}
                 >
-                  ✅ Form submitted successfully.
+                  <ActionStatus
+                    text="Form submitted successfully."
+                    status="success"
+                    iconSize={24}
+                  />
                 </motion.p>
+              )}
+              {formSuccessful === '' && (
+                <ActionStatus text="Sending" status="loading" iconSize={24} />
               )}
             </AnimatePresence>
           </div>
